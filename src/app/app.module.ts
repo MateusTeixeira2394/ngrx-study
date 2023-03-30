8f3586582c7201175397ef7c449874905eaff13a
Mateus Teixeira <mateus.teixeira2394@gmail.com>
2023-03-30 08:41:48 -0300

    Added the new game screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,14 @@
+// Modules imports
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { StoreModule } from '@ngrx/store';
+import {FormsModule} from '@angular/forms'
+
+// Redux imports
+import { statusBarReducer } from './tools/redux/reducers/status-bar.reducer';
+
+// Components imports
 import { AppComponent } from './app.component';
 import { GroundComponent } from './components/ground/ground.component';
 import { BoardComponent } from './components/board/board.component';
@@ -9,8 +16,8 @@ import { HomeComponent } from './views/home/home.component';
 import { StatusBarComponent } from './components/status-bar/status-bar.component';
 import { NewGameFormComponent } from './views/new-game-form/new-game-form.component';
 import { ButtonComponent } from './components/button/button.component';
-import { StoreModule } from '@ngrx/store';
-import { statusBarReducer } from './tools/redux/reducers/status-bar.reducer';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,7 +31,8 @@ import { statusBarReducer } from './tools/redux/reducers/status-bar.reducer';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({statusbar: statusBarReducer})
+    StoreModule.forRoot({statusbar: statusBarReducer}),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/views/new-game-form/new-game-form.component.ts
+++ src/app/views/new-game-form/new-game-form.component.ts
@@ -1,4 +1,6 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import StatusBar from '../../models/status-bar.model';
@@ -20,6 +22,20 @@ export class NewGameFormComponent {
 
   public goToHome(): void {
     this.router.navigate(['home']);
+  }
+
+  public start(form: NgForm): void {
+    console.log(form)
+    if(form.valid){
+      alert('New Game begun!');
+    } else {
+
+      if(form.controls['name']?.errors){
+        document.getElementById('input-name')?.focus();
+      };
+
+    }
+
   }
 
 }
