9cd459212ebd969c698f42dc739bb5232f2b94dc
Mateus Teixeira <mateus.teixeira2394@gmail.com>
2023-03-31 18:49:53 -0300

    Fixed the difficulty of the game

--- src/app/tools/strategies/difficulty.strategy.ts
+++ src/app/tools/strategies/difficulty.strategy.ts
@@ -4,9 +4,9 @@ export default function difficultyStrategy(difficulty: string): Difficulty | und
 
     let map: Map<string, Difficulty> = new Map();
 
-    map.set('easy', { grounds: 44, mines: 20, rowsNCols: 8 });
-    map.set('normal', { grounds: 104, mines: 40, rowsNCols: 12 });
-    map.set('hard', { grounds: 196, mines: 60, rowsNCols: 16 });
+    map.set('easy', { grounds: 52, mines: 12, rowsNCols: 8 });
+    map.set('normal', { grounds: 116, mines: 28, rowsNCols: 12 });
+    map.set('hard', { grounds: 206, mines: 50, rowsNCols: 16 });
 
     return map.get(difficulty);
 
