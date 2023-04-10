9603a434577b9081208d3f84b693429bcc0e09a2
Mateus Teixeira <mateus.teixeira2394@gmail.com>
2023-04-10 14:20:11 -0300

    Added unit tests of board component

--- src/app/components/board/board.component.spec.ts
+++ src/app/components/board/board.component.spec.ts
@@ -1,23 +1,72 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BoardComponent } from './board.component';
+import { GroundComponent } from '../ground/ground.component';
+import Ground from 'src/app/models/ground.model';
 
-describe('BoardComponent', () => {
+describe(BoardComponent.name, () => {
   let component: BoardComponent;
   let fixture: ComponentFixture<BoardComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BoardComponent ]
+      declarations: [BoardComponent, GroundComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(BoardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`should trigger the discoverEvent when the ${BoardComponent.prototype.discover.name} method is called`, () => {
+
+    let eventTriggered: boolean = false;
+
+    const ground: Ground = {
+      isMine: true,
+      known: false,
+      minesAround: 0
+    }
+
+    component.discoverEvent.subscribe(() => eventTriggered = true);
+
+    component.discover(ground);
+
+    expect(eventTriggered).toBe(true);
+
+  });
+
+  const rowAndCols: number = 8;
+
+  it(`should create ${rowAndCols} rows and ${rowAndCols} columns of grounds`, () => {
+
+    let rows: Ground[][] = [];
+
+    for (let i = 0; i < rowAndCols; i++) {
+      let cols: Ground[] = [];
+      for (let j = 0; j < rowAndCols; j++) {
+        cols.push({isMine: true, known: false, minesAround: 0});
+      };
+      rows.push(cols);
+    };
+    
+    component.rows = rows;
+
+    fixture.detectChanges();
+
+    const boardRows: HTMLElement = fixture.nativeElement.querySelector('.board-row');
+    const boardItem: HTMLCollection = boardRows.children
+
+    expect(boardRows.childElementCount).toBe(rowAndCols);
+    expect(boardItem.length).toBe(rowAndCols);
+    
+    
+
+  });
+
 });
