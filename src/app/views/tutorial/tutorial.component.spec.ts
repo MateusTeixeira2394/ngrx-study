1ef70024e44359b6fd48e1301c05465f171194ab
Mateus Teixeira <mateus.teixeira2394@gmail.com>
2023-04-11 18:56:19 -0300

    Added unit tests of tutorial view

--- src/app/views/tutorial/tutorial.component.spec.ts
+++ src/app/views/tutorial/tutorial.component.spec.ts
@@ -1,23 +1,48 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TutorialComponent } from './tutorial.component';
+import { ButtonComponent } from 'src/app/components/button/button.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+import { StoreModule } from '@ngrx/store';
+import { statusBarReducer } from 'src/app/tools/redux/reducers/status-bar.reducer';
 
-describe('TutorialComponent', () => {
+describe(TutorialComponent.name, () => {
   let component: TutorialComponent;
   let fixture: ComponentFixture<TutorialComponent>;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TutorialComponent ]
+      declarations: [ TutorialComponent, ButtonComponent ],
+      imports: [
+        StoreModule.forRoot({
+          statusbar: statusBarReducer
+        }),
+        RouterTestingModule
+      ]
     })
     .compileComponents();
 
+    router = TestBed.get(Router);
     fixture = TestBed.createComponent(TutorialComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`should trigger the router navigate when the ${TutorialComponent.prototype.goToHome} method is called.`, ()=>{
+
+    const spy: jasmine.Spy = spyOn(router,"navigate");
+
+    component.goToHome();
+
+    expect(spy).toHaveBeenCalled();
+
+  });
+
+
 });
