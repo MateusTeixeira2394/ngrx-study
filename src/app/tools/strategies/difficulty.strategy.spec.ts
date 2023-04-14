f4f24e75c632bdfe54bf9fbaab97c49b3779284e
Mateus Teixeira <mateus.teixeira2394@gmail.com>
2023-04-14 10:49:28 -0300

    Fix the unit tests of difficulty strategy

--- src/app/tools/strategies/difficulty.strategy.spec.ts
+++ src/app/tools/strategies/difficulty.strategy.spec.ts
@@ -1,14 +1,14 @@
 import Difficulty from "src/app/models/difficulty.model";
-import difficultyStrategy from "./difficulty.strategy";
+import difficultyStrategy, { EASY_MINES, EASY_ROWS_N_COLS, HARD_MINES, HARD_ROWS_N_COLS, NORMAL_MINES, NORMAL_ROWS_N_COLS } from "./difficulty.strategy";
 import { EASY, HARD, NORMAL } from "src/app/models/game.model";
 
-describe(difficultyStrategy.name, ()=>{
+describe(difficultyStrategy.name, () => {
 
-    const EASY_GROUNDS: number = 52;
-    const NORMAL_GROUNDS: number = 116;
-    const HARD_GROUNDS: number = 206;
+    const EASY_GROUNDS: number = EASY_ROWS_N_COLS * EASY_ROWS_N_COLS - EASY_MINES;
+    const NORMAL_GROUNDS: number = NORMAL_ROWS_N_COLS * NORMAL_ROWS_N_COLS - NORMAL_MINES;
+    const HARD_GROUNDS: number = HARD_ROWS_N_COLS * HARD_ROWS_N_COLS - HARD_MINES;
 
-    it(`Should return the ${EASY} difficulty with ${EASY_GROUNDS} grounds`, ()=>{
+    it(`Should return the ${EASY} difficulty with ${EASY_GROUNDS} grounds`, () => {
 
         const difficultyObj: Difficulty | undefined = difficultyStrategy(EASY);
 
@@ -17,7 +17,7 @@ describe(difficultyStrategy.name, ()=>{
 
     });
 
-    it(`Should return the ${NORMAL} difficulty with ${NORMAL_GROUNDS} grounds`, ()=>{
+    it(`Should return the ${NORMAL} difficulty with ${NORMAL_GROUNDS} grounds`, () => {
 
         const difficultyObj: Difficulty | undefined = difficultyStrategy(NORMAL);
 
@@ -26,7 +26,7 @@ describe(difficultyStrategy.name, ()=>{
 
     });
 
-    it(`Should return the ${HARD} difficulty with ${HARD_GROUNDS} grounds`, ()=>{
+    it(`Should return the ${HARD} difficulty with ${HARD_GROUNDS} grounds`, () => {
 
         const difficultyObj: Difficulty | undefined = difficultyStrategy(HARD);
 
@@ -35,7 +35,7 @@ describe(difficultyStrategy.name, ()=>{
 
     });
 
-    it(`Should return the undefined when pass unknown difficulty`, ()=>{
+    it(`Should return the undefined when pass unknown difficulty`, () => {
 
         const difficultyObj: Difficulty | undefined = difficultyStrategy('unknown-difficulty');
 
@@ -43,8 +43,8 @@ describe(difficultyStrategy.name, ()=>{
 
     });
 
-    
 
-    
+
+
 
 })
